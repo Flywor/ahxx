78948fc7cb4a2c1ac6b13d9c13efde2fa26076d0
luojiantao <244406664@qq.com>
2021-01-11 18:03:21 +0800

    修复类型错误

--- H5-v2/src/components/Pet/info.vue
+++ H5-v2/src/components/Pet/info.vue
@@ -3,12 +3,12 @@
     Lv.{{pet.lv}} {{pet.name}}
   </h3>
   <div v-if="pet.exp">经验：{{pet.exp_c}}/{{pet.exp}}</div>
-  <div><span>HP：{{pet.hp}}</span>（{{formatDouble(pet.grow ? pet.grow[0]: pet.hpGrow)}}）</div>
-  <div><span>MP：{{pet.mp}}</span>（{{formatDouble(pet.grow ? pet.grow[1]: pet.mpGrow)}}）</div>
-  <div><span>物攻：{{pet.atk}}</span>（{{formatDouble(pet.grow ? pet.grow[2]: pet.atkGrow)}}）</div>
-  <div><span>法攻：{{pet.magic}}</span>（{{formatDouble(pet.grow ? pet.grow[3]: pet.magicGrow)}}）</div>
-  <div><span>物防：{{pet.def}}</span>（{{formatDouble(pet.grow ? pet.grow[4]: pet.defGrow)}}）</div>
-  <div><span>速度：{{pet.speed}}</span>（{{formatDouble(pet.grow ? pet.grow[5]: pet.speedGrow)}}）</div>
+  <div><span>HP：{{formatDouble(pet.hp)}}</span>（{{formatDouble(pet.grow ? pet.grow[0]: pet.hpGrow)}}）</div>
+  <div><span>MP：{{formatDouble(pet.mp)}}</span>（{{formatDouble(pet.grow ? pet.grow[1]: pet.mpGrow)}}）</div>
+  <div><span>物攻：{{formatDouble(pet.atk)}}</span>（{{formatDouble(pet.grow ? pet.grow[2]: pet.atkGrow)}}）</div>
+  <div><span>法攻：{{formatDouble(pet.magic)}}</span>（{{formatDouble(pet.grow ? pet.grow[3]: pet.magicGrow)}}）</div>
+  <div><span>物防：{{formatDouble(pet.def)}}</span>（{{formatDouble(pet.grow ? pet.grow[4]: pet.defGrow)}}）</div>
+  <div><span>速度：{{formatDouble(pet.speed)}}</span>（{{formatDouble(pet.grow ? pet.grow[5]: pet.speedGrow)}}）</div>
   <div>法抗：{{formatPercent(pet.magicDef)}}</div>
   <div>技能：{{formatPetSkillName(pet.skills)}}</div>
 </template>
